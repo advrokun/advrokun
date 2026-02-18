2622128b5cd319204cffd0e0ff70694b683081e4
advrokun <rifatbd8272@gmail.com>
2026-02-19 00:26:23 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function scrollToContact(){
+    document.getElementById("contact").scrollIntoView({
+        behavior:"smooth"
+    });
+}
+
+function viewCertificate(){
+    window.open("certificate.jpg","_blank");
+}
